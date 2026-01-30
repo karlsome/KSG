fbb0f86f304e84c50ff22006746fc61b8064ecf8
karlsome <ranilekarlkenneth@sasakicoating.com>
2026-01-30 15:23:30 +0900

    logo bug fix, URL to production

--- public/js/config.js
+++ public/js/config.js
@@ -2,11 +2,11 @@
 // Change URLs here to switch between localhost and production
 
 // For local development:
-const API_URL = 'http://localhost:3000';
-const BASE_URL = 'http://localhost:3000/';
+//const API_URL = 'http://localhost:3000';
+//const BASE_URL = 'http://localhost:3000/';
 
 // For production (uncomment these and comment out localhost):
-// const API_URL = 'https://ksg.freyaaccess.com';
-// const BASE_URL = 'https://ksg.freyaaccess.com/';
+ const API_URL = 'https://ksg.freyaaccess.com';
+ const BASE_URL = 'https://ksg.freyaaccess.com/';
 
 const COMPANY = localStorage.getItem('company') || 'KSG';
